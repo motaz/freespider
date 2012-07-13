57653138a625d365d64caa0c275f9a10b22610c4
motaz <motaz@motaz-T400>
2012-07-13 10:26:51 +0300

    Adding cookies, comments 	modified:   apacheadapter.pas

--- apacheadapter.pas
+++ apacheadapter.pas
@@ -46,7 +46,7 @@ var
 begin
   RequestedHandler := r^.handler;
 
-  { We decline to handle a request if hello-handler is not the value of r->handler }
+  { We decline to handle a request if configured handler is not the value of r^.handler }
   if not SameText(RequestedHandler, HANDLERNAME) then
   begin
     Result := DECLINED;
@@ -54,9 +54,9 @@ begin
   end;
 
   { The following line just prints a message to the errorlog }
-  ap_log_error(PChar(MODULENAME), 54, APLOG_NOERRNO or APLOG_NOTICE,
-   {$ifndef Apache1_3}0,{$endif} r^.server,
-   'mod_hello: %s', [PChar('Before content is output')]);
+  // ap_log_error(PChar(MODULENAME), 54, APLOG_NOERRNO or APLOG_NOTICE,
+  //  {$ifndef Apache1_3}0,{$endif} r^.server,
+  //  'mod_hello: %s', [PChar('Before content is output')]);
 
   { We set the content type before doing anything else }
 
@@ -88,6 +88,8 @@ begin
     end;
 
     web:= WebModule.Create(nil);
+
+    // Search for SpiderApache component in Web Data Module
     with web do
     for i:= 0 to ComponentCount - 1 do
     if Components[i] is TSpiderApache then
@@ -97,22 +99,42 @@ begin
       ContentType:= apr_table_get(r^.headers_in, 'CONTENT-TYPE');
       if Trim(ContentType) = '' then
         ContentType:= r^.content_type;
+
+      // Intialize SpiderApache:
       SpiderApacheObj.Init(r^.path_info, ContentType, r^.method, r^.args, apr_table_get(r^.headers_in, 'COOKIE'),
       apr_table_get(r^.headers_in, 'User-Agent'), Posteddata, apr_table_get(r^.headers_in, 'Content-Length'),
         apr_table_get(r^.headers_in, 'REFERER'), r^.connection^.remote_ip, r^.uri, ap_get_server_version);
+
+      // Execute web application
       aResponse:= SpiderApacheObj.Execute;
+
+      // Send response to the browser
       if Assigned(aResponse) then
       begin
+        // Content type
         ap_set_content_type(r, PChar(aResponse.ContentType));
+
+        // Custome header
         with aResponse.CustomHeader do
         for j:= 0 to Count - 1 do
           apr_table_set(r^.headers_out, PChar(Copy(Strings[j], 1, Pos(':', Strings[j]) - 1)),
             PChar(Copy(Strings[j], Pos(':', Strings[j]) + 1, Length(Strings[j])) ));
+
+        // Response contents
         DataLen:= Length(aResponse.Content.Text);
         Data:= GetMem(DataLen);
         Move(Pointer(aResponse.Content.Text)^, Data^, DataLen);
         ap_rwrite(Data, DataLen, r);
+
+        // Set cookies:
+        with aResponse.CookieList do
+        for j:= 0 to Count - 1 do
+          apr_table_set(r^.headers_out, 'Set-Cookie', PChar(Copy(Strings[j],
+            Pos(':', Strings[j]) + 1, Length(Strings[j]))));
+
       end;
+
+
       Break;
     end;
 
@@ -121,52 +143,6 @@ begin
     ap_rputs(PChar('<br/> Error in WebModule : <font color=red>' + e.Message + '</font>'), r);
   end;
   web.Free;
-
- { ap_rputs(PChar('query: <font color=gray>' + r^.args + '</font>'), r);
-  ap_rputs(PChar('<br/>Hostname: <font color=gray>' +  r^.hostname + '</font>'), r);
-  ap_rputs(PChar('<br/>Method: <font color=gray>' +  r^.method + '</font>'), r);
-  ap_rputs(PChar('<br/>the request: <font color=gray>' +  r^.the_request + '</font>'), r);
-  ap_rputs(PChar('<br/>Protocol: <font color=gray>' +  r^.protocol + '</font>'), r);
-  ap_rputs(PChar('<br/>Content Type: <font color=gray>' +  r^.content_type + '</font>'), r);
-  ap_rputs(PChar('<br/>Encoding: <font color=gray>' +  r^.content_encoding + '</font>'), r);
-  ap_rputs(PChar('<br/>Auth type: <font color=gray>' +  r^.ap_auth_type + '</font>'), r);
-  ap_rputs(PChar('<br/>User: <font color=gray>' +  r^.user + '</font>'), r);
-  ap_rputs(PChar('<br/>Status Line: <font color=gray>' +  r^.status_line + '</font>'), r);
-  ap_rputs(PChar('<br/>VList Validator: <font color=gray>' +  r^.vlist_validator + '</font>'), r);
-  ap_rputs(PChar('<br/>URI: <font color=gray>' +  r^.uri+ '</font>'), r);
-  ap_rputs(PChar('<br/>Range: <font color=gray>' +  r^.range+ '</font>'), r);
-  ap_rputs(PChar('<br/>Path Info: <font color=gray>' +  r^.path_info+ '</font>'), r);
-  ap_rputs(PChar('<br/>Canonical Filename: <font color=gray>' +  r^.canonical_filename+ '</font>'), r);
-  ap_rputs(PChar('<br/>Handler: <font color=gray>' +  r^.handler + '</font>'), r);
-  ap_rputs(PChar('<br/>show_mpm: <font color=gray>' + ap_show_mpm + '</font>'), r);
-  ap_rputs(PChar('<br/>auth_type: <font color=gray>' +  ap_auth_type(r) + '</font>'), r);
-  ap_rputs(PChar('<br/>auth_name: <font color=gray>' +  ap_auth_name(r) + '</font>'), r);
-  ap_rputs(PChar('<br/>server description: <font color=gray>' +  ap_get_server_description + '</font>'), r);
-  ap_rputs(PChar('<br/>remote longname: <font color=gray>' +  ap_get_remote_logname(r) + '</font>'), r);
-  ap_rputs(PChar('<br/>Status: <font color=gray>' +  IntToStr(r^.status) + '</font>'), r);
-
-  ap_rputs(PChar('<br/>User-Agent: <font color=gray>' +  apr_table_get(r^.headers_in, 'User-Agent') + '</font>'), r);
-
-  // Read cookies:
-  ap_rputs(PChar('<br/>Cookies: <font color=gray>' +  apr_table_get(r^.headers_in, 'COOKIE') + '</font>'), r);
-
-  // Set cookies:
-  apr_table_set(r^.headers_out, 'Set-Cookie', 'sessionid=122');
-
-
-  // Retrieve posted data
-  if (r^.method = 'POST') then
-  begin
-    ap_setup_client_block(r, REQUEST_CHUNKED_ERROR);
-    ap_rputs('contents:', r);
-    Buf:= StrAlloc(1024);
-    repeat
-       NumRead:= ap_get_client_block(r, Buf, 1024);
-       ap_rputs(PChar(Copy(Buf, 0, NumRead)), r);
-    until NumRead = 0;
-
-  end;}
-
 
   Result:= Ok;
 end;
